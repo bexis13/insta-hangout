7fab72d10680b83ca02fc76ac533c50cbe3d1070
Abose Ukhun <bexis17@gmail.com>
2017-10-10 19:29:00 +0000

    add location as a geoJSON object to the user schema

--- models/user.js
+++ models/user.js
@@ -4,7 +4,11 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
     name : String,
     email: String,
-    password : String
+    password : String,
+    location: {
+      type: "Point",
+      coordinates: []
+    }
 });
 
 UserSchema.plugin(passportLocalMongoose);
